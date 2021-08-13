7fae6c4f9f58e79bc845f33fa0953bfe94c7529e
kiba518 <liujin_hzzh@126.com>
2021-08-13 10:01:49 +0800

    ArcGisJs4

---
+++ WebArcGis_JS4/Pages/MapInterNet_FeatureLayer.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebArcGis_JS4.Pages
+{
+    public class MapInterNet_FeatureLayerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
